be1cbe3c3653ba791309c50360008f66b2565173
Varun Mathur <t-vamath@microsoft.com>
2018-01-27 21:47:53 -0500

    added question routing logic

--- app.py
+++ app.py
@@ -38,9 +38,14 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    message_text = messaging_event["message"]["text"].lower()  # the message's text
 
-                    send_message(sender_id, "roger that!")
+                    if("can you notify me when a class becomes open" in message_text):
+                        send_message(sender_id, "Sure! Which class?")
+                    elif("what can you do" in message_text):
+                        send_message("try asking: can you notify me when a class becomes open?")
+                    else:
+                        send_message("I didn't get that. Trying asking me what I can do.")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
